501e008b88044ab6210354894364c38bd0501584
Nicholas Moore <nsmoore57@gmail.com>
2020-01-21 22:51:57 -0600

    Updating Newton.py
    - Trying to debug
    - fix bug in step size method of Newton
    - Newton test works for arctan functions
    - Not yet working for Rosenbrock
    - better syntax

--- NewtonsMethod/Newton.py
+++ NewtonsMethod/Newton.py
@@ -4,35 +4,56 @@ import numpy.linalg as LA
 import numpy as np
 
 def Newton(f, x0, tao, c, tol, kmax):
-    """Run Newton's Rootfinding method to find the zero of f"""
+    """
+    Run Newton's Rootfinding method to find the zero of f
+    """
     k = 1
     s = LA.norm(f(x0))**2
-    xk = x0
-    while s > tol and k < kmax:
-        H = _CentralDifferences(f,xk,tao)
+    xk = x0.copy() # We don't want to change x0 from the outside scope
 
-        # Uses LAPACK _gesv
-        dk = LA.solve(H,-f(xk))
+    while np.sqrt(s) > tol and k < kmax:
+        # Build the Jacobian matrix approximation using Central Differences
+        H = _CentralDifferences(f, xk, tao)
+
+        try:
+            # Uses LAPACK _gesv to solve H*dk = -f(xk)
+            dk = LA.solve(H, -f(xk))
+        except LA.LinAlgError:
+            # Most likely here because the above H is singular
+            # Therefore we regularize by adding a small (10^-7) multiple of I:
+            theta = 10**(-7)
+            dk = LA.solve(H + theta*np.eye(xk.shape[0]), -f(xk))
 
         z = c*LA.norm(f(xk).transpose()*H)*LA.norm(dk)
 
+        # Solve the step size using the method by Stoer and Bulirsch
         j = 0
         L = LA.norm(f(xk + dk))**2
         R = s-z
         Lmin = L
         index = 0
         while L > R:
-            j = j + 1
+            j += 1
             L = LA.norm(f(xk + 2**(-j)))**2
-            R = s = 2**(-j)*z
+            R = s - 2**(-j)*z
             if L < Lmin:
                 Lmin = L
                 index = j
 
+        # print("dk = " + str(dk))
+        # print("index = " + str(index))
+        # Update xk
         xk = xk + 2**(-index)*dk
-        k = k + 1
+        # print("xk = " + str(xk))
+        k += 1
         s = LA.norm(f(xk))**2
 
+    # If kmax gets exceeded, we can't trust the answer so return None
+    if k >= kmax:
+        print("k exceeded kmax, can't trust answer")
+        return None
+
+    # Otherwise, we stopped the above loop because we're within tolerance so the answer is good
     return xk
 
 def GradDescent(f, x0, tol, kmax):
@@ -42,36 +63,69 @@ def GradDescent(f, x0, tol, kmax):
     xnew = x0
     dold = np.zeros(x0.shape)
     while LA.norm(f(xnew)) > tol and k < kmax:
+        # Step direction
         dnew = -f(xnew)
+
+        # Step size
         gamma = abs(np.matmul((xnew - xold).transpose(), -dnew + dold))/LA.norm(-dnew+dold)**2
+
+        # Reset all old values to avoid evaluating f more times than necessary
         xold = xnew
         dold = dnew
         xnew = xold + gamma*dnew
-        k = k+1
+        k += 1
+
+    # If kmax gets exceeded, we can't trust the answer so return None
     if k >= kmax:
-        print("kmax exceeded - don't trust the answer")
+        print("k exceeded kmax, can't trust answer")
+        return None
+
+    # Otherwise, we stopped the above loop because we're within tolerance so the answer is good
     return xnew
 
-def _CentralDifferences(f,x,tao):
+def _CentralDifferences(f, x, tao):
     """
     Calculates an approximation to the Jacobian based on Central Differences:
     -------------------------------------------------------------------------
     f: function to approximate the Jacobian of
     x: the point to approximate the Jacobian around
     tao: 2*tao is the length of secant line between central difference points
-    
+
     H(:,i) = 1/(2tao) (f(x + tao*ei) - f(x - tao*ei))
-    
-    TODO: Rewrite to only perturb one coord at a time instead of adding bunch of zeros
     """
-    H = np.eye(x.shape[0])
-    f_x = f(x)
+    H = np.empty((x.shape[0], x.shape[0]))
     for i in range(x.shape[0]):
         xhigh = x.copy()
         xlow = x.copy()
-        xhigh[i] = xhigh[i] + tao
-        xlow[i] = xlow[i] - tao
-        
-        H[:,i] = 1/(2*tao)*(f(xhigh) - f(xlow))
+        xhigh[i] += tao
+        xlow[i] -= tao
+
+        H[:, i] = (1/(2*tao)*(f(xhigh) - f(xlow))).flatten()
     return H
 
+if __name__ == "__main__":
+    def Grad_Rosenbrock(a, x):
+        """
+        Gradient of the Rosenbrock function
+        Used for obtaining the mininum of the Rosenbrock
+        """
+        dfdx1 = -2*(a-x[0]) - 400*(x[1]-x[0]**2)*x[0]
+        dfdx2 = 200*(x[1]-x[0]**2)
+        return np.vstack((dfdx1, dfdx2))
+
+    Rosenbrock1 = (lambda x: Grad_Rosenbrock(1, x))
+
+    def arctan2d(x):
+        """
+        2D arctan function for testing GradDescent
+        """
+        res1 = np.arctan(x[0] - np.pi/4)
+        res2 = np.arctan(x[1] - np.pi/4)
+        return np.vstack((res1, res2))
+
+    x0 = np.array([[1.01], [1.01]], dtype="float")
+    tao = 10**(-5)
+    c = 0.5
+    tol = 10**(-2)
+    kmax = 100000
+    print(Newton(Rosenbrock1, x0, tao, c, tol, kmax))
--- NewtonsMethod/test_Newton.py
+++ NewtonsMethod/test_Newton.py
@@ -12,13 +12,14 @@ def Rosenbrock(a, x):
     """
     res = (a - x[0])**2 + 100*(x[1]-x[0]**2)**2
     return np.array([res, res])
-def Rosenbrock_Jac(a, x):
+def Grad_Rosenbrock(a, x):
     """
-    Function for testing optimization, Jacobian of Rosenbrock function
+    Gradient of the Rosenbrock function
+    Used for obtaining the mininum of the Rosenbrock
     """
-    df1dx1 = -2*(a-x[0]) - 400*(x[1]-x[0]**2)*x[0]
-    df1dx2 = 200*(x[1]-x[0])
-    return np.array([[df1dx1, df1dx2], [df1dx1, df1dx2]])
+    dfdx1 = -2*(a-x[0]) - 400*(x[1]-x[0]**2)*x[0]
+    dfdx2 = 200*(x[1]-x[0]**2)
+    return np.vstack((dfdx1, dfdx2))
 
 def arctan2d(x):
     """
@@ -28,28 +29,28 @@ def arctan2d(x):
     res2 = np.arctan(x[1] - np.pi/4)
     return np.vstack((res1, res2))
 
-Rosenbrock1 = (lambda x: Rosenbrock(1, x))
-Rosenbrock2 = (lambda x: Rosenbrock(2, x))
-Rosenbrock3 = (lambda x: Rosenbrock(3, x))
+Rosenbrock1 = (lambda x: Grad_Rosenbrock(1, x))
+Rosenbrock2 = (lambda x: Grad_Rosenbrock(2, x))
+Rosenbrock3 = (lambda x: Grad_Rosenbrock(3, x))
 arctan1d = (lambda x: np.arctan(x - np.pi/4))
 
 def test_NewtonsMethod():
     """Test Newton.Newton function"""
-    x0 = np.array([10])
+    x0 = np.array([2], dtype="float")
     tao = 10**(-5)
-    c = 10*(-6)
+    c = 0.5
     tol = 10**(-6)
     kmax = 1000
     assert arctan1d(Newton.Newton(arctan1d, x0, tao, c, tol, kmax)) < tol
 
-    x0 = np.array([[1], [1]])
+    x0 = np.array([[3], [3]], dtype="float")
     tao = 10**(-5)
-    c = 10*(-6)
-    tol = 10**(-6)
+    c = 0.5
+    tol = 10**(-1)
     kmax = 1000
-    # assert Newton.Newton(Rosenbrock1, x0, tao, c, tol, kmax) is None
-    # assert Newton.Newton(Rosenbrock2, x0, tao, c, tol, kmax) is None
-    # assert Newton.Newton(Rosenbrock3, x0, tao, c, tol, kmax) is None
+    assert LA.norm(Rosenbrock1(Newton.Newton(Rosenbrock1, x0, tao, c, tol, kmax))) < tol
+    assert LA.norm(Rosenbrock1(Newton.Newton(Rosenbrock2, x0, tao, c, tol, kmax))) < tol
+    assert LA.norm(Rosenbrock1(Newton.Newton(Rosenbrock3, x0, tao, c, tol, kmax))) < tol
 
 def test_GradDescent():
     """Test Newton.GradDescent function"""
