2d2de77af9099bb32747acda89329d821d408002
Nicholas Moore <nicholas.s.moore@ttu.edu>
2020-02-18 14:34:04 -0600

    Add another test

--- test_InteriorPoint.py
+++ test_InteriorPoint.py
@@ -6,6 +6,9 @@ import numpy.linalg as LA
 import InteriorPoint as IP
 
 def test_InteriorPointBarrier():
+    """Tests the IP.InteriorPointBarrier Method"""
+    # TODO: Fix tests to be truer to the expected convergence behavior
+    
     A = np.array([[1, 2, -1, 1],[2, -2, 3, 3],[1, -1, 2, -1]],dtype="float")
     b = np.array([[0, 9, 6]]).transpose()
     c = np.array([[-3, 1, 3, -1]]).transpose()
@@ -17,4 +20,12 @@ def test_InteriorPointBarrier():
    
     x,lamb,s = IP.InteriorPointBarrier(A, b, c, tol, kmax, rho, mu0, mumin)
     true_Opt = np.array([[1, 1, 3, 0]]).transpose()
-    assert LA.norm(x - true_Opt) < 1.2*tol
+    assert LA.norm(x - true_Opt) < 10*tol
+    
+    A = np.array([[5, -4, 13, -2, 1],[1, -1, 5, -1, 1]],dtype="float")
+    b = np.array([[20, 8]]).transpose()
+    c = np.array([[1, 6, -7, 1, 5]]).transpose()
+    
+    x,lamb,s = IP.InteriorPointBarrier(A, b, c, tol, kmax, rho, mu0, mumin)
+    true_Opt = np.array([[0, 0.5714, 1.7143, 0, 0]]).transpose()
+    assert LA.norm(x - true_Opt) < 10*tol
