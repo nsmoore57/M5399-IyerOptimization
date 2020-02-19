7f28a9cc59e6da9f888cf0569cea63e861b15985
Nicholas Moore <nicholas.s.moore@ttu.edu>
2020-02-19 10:51:53 -0600

    Barrier - Adding ability to optimize quadratic programming problems

--- test_InteriorPoint.py
+++ test_InteriorPoint.py
@@ -11,20 +11,22 @@ def test_InteriorPointBarrier():
     A = np.array([[1, 2, -1, 1],[2, -2, 3, 3],[1, -1, 2, -1]],dtype="float")
     b = np.array([[0, 9, 6]]).transpose()
     c = np.array([[-3, 1, 3, -1]]).transpose()
+    Q = np.zeros((4,4))
     tol = 1e-5
     kmax = 10000
     rho = .9
     mu0 = 1e4
     mumin = 1e-8
 
-    x,k = IP.InteriorPointBarrier(c, A, b, tol, kmax, rho, mu0, mumin)
+    x,k = IP.InteriorPointBarrier(Q, c, A, b, tol, kmax, rho, mu0, mumin)
     true_Opt = np.array([[1, 1, 3, 0]]).transpose()
     assert LA.norm(x - true_Opt) < 10*tol and LA.norm(np.matmul(A,x)-b) < tol
 
     A = np.array([[5, -4, 13, -2, 1],[1, -1, 5, -1, 1]],dtype="float")
     b = np.array([[20, 8]]).transpose()
     c = np.array([[1, 6, -7, 1, 5]]).transpose()
+    Q = np.zeros((5,5))
 
-    x,k = IP.InteriorPointBarrier(c, A, b, tol, kmax, rho, mu0, mumin)
+    x,k = IP.InteriorPointBarrier(Q, c, A, b, tol, kmax, rho, mu0, mumin)
     true_Opt = np.array([[0, 0.5714, 1.7143, 0, 0]]).transpose()
     assert LA.norm(x - true_Opt) < 10*tol and LA.norm(np.matmul(A,x) - b) < tol
