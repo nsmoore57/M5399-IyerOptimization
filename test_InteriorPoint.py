20b6123f11f9affb1fc09c8927de7d90cbb2df98
Nicholas Moore <nicholas.s.moore@ttu.edu>
2020-02-18 23:50:42 -0600

    Improved IPB tests to check whether constraints are within tolerance

--- test_InteriorPoint.py
+++ test_InteriorPoint.py
@@ -7,7 +7,6 @@ import InteriorPoint as IP
 
 def test_InteriorPointBarrier():
     """Tests the IP.InteriorPointBarrier Method"""
-    # TODO: Fix tests to be truer to the expected convergence behavior
 
     A = np.array([[1, 2, -1, 1],[2, -2, 3, 3],[1, -1, 2, -1]],dtype="float")
     b = np.array([[0, 9, 6]]).transpose()
@@ -20,7 +19,7 @@ def test_InteriorPointBarrier():
 
     x,k = IP.InteriorPointBarrier(c, A, b, tol, kmax, rho, mu0, mumin)
     true_Opt = np.array([[1, 1, 3, 0]]).transpose()
-    assert LA.norm(x - true_Opt) < 10*tol
+    assert LA.norm(x - true_Opt) < 10*tol and LA.norm(np.matmul(A,x)-b) < tol
 
     A = np.array([[5, -4, 13, -2, 1],[1, -1, 5, -1, 1]],dtype="float")
     b = np.array([[20, 8]]).transpose()
@@ -28,4 +27,4 @@ def test_InteriorPointBarrier():
 
     x,k = IP.InteriorPointBarrier(c, A, b, tol, kmax, rho, mu0, mumin)
     true_Opt = np.array([[0, 0.5714, 1.7143, 0, 0]]).transpose()
-    assert LA.norm(x - true_Opt) < 10*tol
+    assert LA.norm(x - true_Opt) < 10*tol and LA.norm(np.matmul(A,x) - b) < tol
